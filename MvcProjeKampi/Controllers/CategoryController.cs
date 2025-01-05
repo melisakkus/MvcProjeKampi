398bb8c4003d651e3d13d35d839f467d74e8aec9
Melisa Akkuş <melisa.akkus01@gmail.com>
2025-01-05 21:32:54 +0300

    Add project files.

---
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Categoryt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
