feaaaedfe6dcb238f70a35360e3952ee66bef03a
Melisa Akkuş <melisa.akkus01@gmail.com>
2025-03-19 22:08:05 +0300

    updates

--- MvcProjeKampi/Controllers/AdminCategoryController.cs
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules_Fluent;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +13,36 @@ namespace MvcProjeKampi.Controllers
 {
     public class AdminCategoryController : Controller
     {
-        // GET: AdminCategory
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
+            var values = categoryManager.GetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category model)
+        {
+            CategoryValidator validator = new CategoryValidator();
+            ValidationResult result = validator.Validate(model);
+            if (result.IsValid)
+            {
+                categoryManager.CategoryAddBL(model);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach(var errors in result.Errors)
+                {
+                    ModelState.AddModelError(errors.PropertyName, errors.ErrorMessage);
+                }
+            }
             return View();
         }
     }
